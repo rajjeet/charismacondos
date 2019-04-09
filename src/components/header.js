5fc0a7251bc9e68f9ef152a6e0a963192dc6e804
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-04-09 19:48:08 -0400

    Styled a basic hero headline

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,13 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css";
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `white`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `black`,
-            textDecoration: `none`,
-          }}
-        >
+  <header>
+    <div>
+      <h1>
+        <Link id={'home-link'} to="/">
           {siteTitle}
         </Link>
       </h1>
