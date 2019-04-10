53cf1cdec7e4709cb7509acdbfd066004749dea6
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-04-10 08:21:26 -0400

    Added floor plan section and buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,45 +1,95 @@
-import React from "react"
+import React, { Component } from "react"
 import "./index.css"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { graphql } from "gatsby"
 import Img from "gatsby-image"
+import * as PropTypes from "prop-types"
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
-    <section id={'banner'}>
-      <table id={"hero-banner"}>
-        <tbody>
-        <tr id={"main-heading"}>
-          <td>Gateway To the GTA</td>
-        </tr>
-        <tr id={"supporting-headline"}>
-          <td>Where dreams come true</td>
-        </tr>
-        <tr id={"call-to-action-top"}>
-          <td>
-            <button>Reserve Your Spot</button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-    </section>
-    <section id={'selling-features'}>
-      {/*<Img fluid={data.placeholderImage.childImageSharp.fluid}/>*/}
-    </section>
-  </Layout>
-)
+const myFunction = (event) => {
+  let id = event.target.id.replace("fp-btn-", "")
+  let ele = document.getElementById(id)
+  let floorPlans = document.getElementsByClassName("floor-plan")
+  Object.keys(floorPlans).forEach(key => floorPlans[key].style.display = "none")
+  console.log(ele)
+  ele.style.display = "block"
+}
+
+class IndexPage extends Component {
+  render() {
+    let { data } = this.props
+    return (
+      <Layout>
+        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
+        <section id={"banner"}>
+          <table id={"hero-banner"}>
+            <tbody>
+            <tr id={"main-heading"}>
+              <td>Gateway To the GTA</td>
+            </tr>
+            <tr id={"supporting-headline"}>
+              <td>Where dreams come true</td>
+            </tr>
+            <tr id={"call-to-action-top"}>
+              <td>
+                <button>Reserve Your Spot</button>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </section>
+        <section id={"gallery"}>
+
+          <div id={"floor-plan-buttons"}>
+            <div id={"fp-btn-studio"} onClick={myFunction}>Studio SA</div>
+            <div id={"fp-btn-suite1ad"} onClick={myFunction}>Suite 1 AD</div>
+          </div>
+            <div id={"studio"} className="floor-plan">
+              <Img fluid={data.floorPlanStudioSA.childImageSharp.fluid}/>
+            </div>
+            <div id={"suite1ad"} className={"floor-plan"}>
+              <Img fluid={data.floorPlanSuite1AD.childImageSharp.fluid}/>
+            </div>
+        </section>
+      </Layout>
+    )
+  }
+}
+
+IndexPage.propTypes = { data: PropTypes.any }
 
 export const query = graphql`
   query {
-    placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+    floorPlanStudioSA: file(relativePath: { eq: "Mobilio-Condos-Studio-SA-floorplan-v3.jpg" }) {
       childImageSharp {
         fluid(maxWidth: 300) {
           ...GatsbyImageSharpFluid
         }
       }
     }
+    floorPlanSuite1AD: file(relativePath: { eq: "Mobilio-Condos-Suite-1A-D-floorplan-v3.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    floorPlanSuite1A: file(relativePath: { eq: "Mobilio-Condos-Suite-1A-floorplan-v3.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    floorPlanSuite1BD: file(relativePath: { eq: "Mobilio-Condos-Suite-1B-D-floorplan-v3.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 300) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    
+    
   }
 `
 
