598b30db4ffc7d49db2f52400c31dd826feb58dd
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-04-12 06:09:37 -0400

    Refactored amentities

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,6 +3,14 @@ import "./Location.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFilm, faSquare, faGlassMartiniAlt, faDumbbell, faBaby, faCloudSun, faRunning } from "@fortawesome/free-solid-svg-icons"
 
+function getAmenity(frontIcon, text) {
+  return <div className="fa-layers">
+    <FontAwesomeIcon className={"amenitiesBackIcon"} icon={faSquare} size={"3x"} color={"darkred"}/>
+    <FontAwesomeIcon className={"amenitiesFrontIcon"} icon={frontIcon} size={"2x"} color={"white"}/>
+    <div className={"amenity-text"}>{text}</div>
+  </div>
+}
+
 export const Location = () => (
   <section id={"location"}>
     <h1>Location</h1>
@@ -41,42 +49,18 @@ export const Location = () => (
         <div>Dining</div>
       </div>
     </div>
-    <iframe title={"googlemaps"} width="600" height="450"
+    <iframe title={"googlemaps"} height="450"
             src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJvdrsxLkvK4gRg5m2IsjTerw&key=AIzaSyBwH66e9iE5lT856EiEJne_wcgBjJkh2uA"
             allowFullScreen/>
     <div id={"amenities"}>
       <h1>Amenities</h1>
-      <p>Live with luxury, convienance, and comfort</p>
-      <div className="fa-layers">
-        <FontAwesomeIcon className={"amenitiesBackIcon"} icon={faSquare} size={"3x"} color={"darkred"}/>
-        <FontAwesomeIcon className={"amenitiesFrontIcon"} icon={faFilm} size={"2x"} color={"white"}/>
-        <div className={'amenity-text'}>Theatre Room</div>
-      </div>
-      <div className="fa-layers">
-        <FontAwesomeIcon className={"amenitiesBackIcon"} icon={faSquare} size={"3x"} color={"darkred"}/>
-        <FontAwesomeIcon className={"amenitiesFrontIcon"} icon={faGlassMartiniAlt} size={"2x"} color={"white"}/>
-        <div className={'amenity-text'}>Party Room with Bar and Kitchen</div>
-      </div>
-      <div className="fa-layers">
-        <FontAwesomeIcon className={"amenitiesBackIcon"} icon={faSquare} size={"3x"} color={"darkred"}/>
-        <FontAwesomeIcon className={"amenitiesFrontIcon"} icon={faCloudSun} size={"2x"} color={"white"}/>
-        <div className={'amenity-text'}>Outdoor Terrace with Lounge, Fire Pit and BBQs</div>
-      </div>
-      <div className="fa-layers">
-        <FontAwesomeIcon className={"amenitiesBackIcon"} icon={faSquare} size={"3x"} color={"darkred"}/>
-        <FontAwesomeIcon className={"amenitiesFrontIcon"} icon={faRunning} size={"2x"} color={"white"}/>
-        <div className={'amenity-text'}>Virtual & Yoga/Aerobics Studios</div>
-      </div>
-      <div className="fa-layers">
-        <FontAwesomeIcon className={"amenitiesBackIcon"} icon={faSquare} size={"3x"} color={"darkred"}/>
-        <FontAwesomeIcon className={"amenitiesFrontIcon"} icon={faBaby} size={"2x"} color={"white"}/>
-        <div className={'amenity-text'}>Kids Play Room and Outdoor Play Area</div>
-      </div>
-      <div className="fa-layers">
-        <FontAwesomeIcon className={"amenitiesBackIcon"} icon={faSquare} size={"3x"} color={"darkred"}/>
-        <FontAwesomeIcon className={"amenitiesFrontIcon"} icon={faDumbbell} size={"2x"} color={"white"}/>
-        <div className={'amenity-text'}>Fitness Centre</div>
-      </div>
+      <p>Live with luxury, convenience, and comfort</p>
+      {getAmenity(faFilm, "Theatre Room")}
+      {getAmenity(faGlassMartiniAlt, "Party Room with Bar and Kitchen")}
+      {getAmenity(faCloudSun, "Outdoor Terrace with Lounge, Fire Pit and BBQs")}
+      {getAmenity(faRunning, "Virtual & Yoga/Aerobics Studios")}
+      {getAmenity(faBaby, "Kids Play Room and Outdoor Play Area")}
+      {getAmenity(faDumbbell, "Fitness Centre")}
     </div>
 
   </section>
