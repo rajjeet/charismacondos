2cbc8824abe305d1795af7a50739a214ad33795b
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-04-12 06:51:44 -0400

    Added call to action button in banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,10 +3,12 @@ import "./Banner.css"
 
 export const Banner = () => (
   <section id={"banner-section"}>
-    <div className={'content'}>
-    <h6>Book While They Last</h6>
-    <p>Mobilio Condos unit sizes range from 401 to 739 square feet, with pricing ranging between mid $300s to mid $600,
-      and are scheduled to finish 2022.</p>
+    <div className={"content"}>
+      <h6>Limited Units So Book Fast</h6>
+      <p>Mobilio Condos unit sizes range from 401 to 739 square feet, with pricing ranging between mid $300s to mid
+        $600,
+        and are scheduled to finish 2022.</p>
+      <button className="call-to-action-btn">Book Now</button>
     </div>
   </section>
 )
