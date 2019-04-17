9ddf86f16cf752efa20032157afef17d47555771
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-04-17 06:04:24 -0400

    Fixed Gatsby build issues relating to browser object: window and document

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,45 @@
 import { graphql, StaticQuery } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { Component } from "react"
 import "./Header.css"
 import GatsbyImage from "gatsby-image"
 
 function deactivateMenuLinks() {
-  let linkNodes = document.getElementById("header-links").childNodes
-  linkNodes.forEach(linkNode => linkNode.classList.remove("active"))
-}
-
-window.onscroll = () => {
-  const header = document.getElementById("page-header")
-  if (header) {
-    const sticky = header.offsetTop
-    if (window.pageYOffset > sticky) {
-      header.classList.add("sticky")
-    } else {
-      header.classList.remove("sticky")
-    }
+  if (typeof document !== undefined) {
+    let linkNodes = document.getElementById("header-links").childNodes
+    linkNodes.forEach(linkNode => linkNode.classList.remove("active"))
   }
-
 }
 
 const activateLink = (event) => {
-  let checkbox = document.getElementById("menu-toggle-checkbox")
-  if (checkbox.checked) checkbox.checked = false
-  deactivateMenuLinks()
-  event.target.classList.add("active")
+  if (typeof document !== undefined) {
+    let checkbox = document.getElementById("menu-toggle-checkbox")
+    if (checkbox.checked) checkbox.checked = false
+    deactivateMenuLinks()
+    event.target.classList.add("active")
+  }
 }
 
-const Header = () => (
-  <StaticQuery query={graphql`
+class Header extends Component {
+
+  componentDidMount() {
+    window.onscroll = () => {
+      const header = document.getElementById("page-header")
+      if (header) {
+        const sticky = header.offsetTop
+        if (window.pageYOffset > sticky) {
+          header.classList.add("sticky")
+        } else {
+          header.classList.remove("sticky")
+        }
+      }
+    }
+
+  }
+
+  render() {
+    return (
+      <StaticQuery query={graphql`
     query {
       mobilioLogo: file(relativePath: { eq: "mobilio_logo.png" }) {
         childImageSharp {
@@ -41,45 +50,47 @@ const Header = () => (
       }
      }
    `} render={data => (
-    <div id={"page-header"} className="">
-      <div id={"main-logo"}>
-        <a href={"/#"}>
-          <GatsbyImage fluid={data.mobilioLogo.childImageSharp.fluid}/>
-        </a>
-      </div>
-      <div id={"header-links"}>
-        <a href={"#photo-gallery"} onClick={activateLink}>Gallery</a>
-        <a href={"#location"} onClick={activateLink}>Location</a>
-        <a href={"#amenities"} onClick={activateLink}>Amenities</a>
-        <a href={"#floor-plans"} onClick={activateLink}>Floor Plans</a>
-      </div>
-      <div id={"menu-toggle"}>
-        <input id={"menu-toggle-checkbox"} type={"checkbox"}/>
-        <span/>
-        <span/>
-        <span/>
-        <ul id={"menu"}>
-          <a href={"#photo-gallery"} onClick={activateLink}>
-            <li>Gallery</li>
-          </a>
-          <a href={"#location"} onClick={activateLink}>
-            <li>Location</li>
-          </a>
-          <a href={"#amenities"} onClick={activateLink}>
-            <li>Amenities</li>
-          </a>
-          <a href={"#floor-plans"} onClick={activateLink}>
-            <li>Floor Plans</li>
-          </a>
-        </ul>
-      </div>
-    </div>
+        <div id={"page-header"} className="">
+          <div id={"main-logo"}>
+            <a href={"/#"}>
+              <GatsbyImage fluid={data.mobilioLogo.childImageSharp.fluid}/>
+            </a>
+          </div>
+          <div id={"header-links"}>
+            <a href={"#photo-gallery"} onClick={activateLink}>Gallery</a>
+            <a href={"#location"} onClick={activateLink}>Location</a>
+            <a href={"#amenities"} onClick={activateLink}>Amenities</a>
+            <a href={"#floor-plans"} onClick={activateLink}>Floor Plans</a>
+          </div>
+          <div id={"menu-toggle"}>
+            <input id={"menu-toggle-checkbox"} type={"checkbox"}/>
+            <span/>
+            <span/>
+            <span/>
+            <ul id={"menu"}>
+              <a href={"#photo-gallery"} onClick={activateLink}>
+                <li>Gallery</li>
+              </a>
+              <a href={"#location"} onClick={activateLink}>
+                <li>Location</li>
+              </a>
+              <a href={"#amenities"} onClick={activateLink}>
+                <li>Amenities</li>
+              </a>
+              <a href={"#floor-plans"} onClick={activateLink}>
+                <li>Floor Plans</li>
+              </a>
+            </ul>
+          </div>
+        </div>
 
-  )
-  }/>
+      )
+      }/>
 
 
-)
+    )
+  }
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,32 +1,34 @@
-import React from "react"
+import React, { Component } from "react"
 import "./PhotoGallery.css"
 import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
 let slideIndex = 1
 let slideshowTimeout
-jumpToSlide(1)
+
 
 const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step))
 
 export function jumpToSlide(selection, isUserClick = false) {
-  let index
-  const slides = document.getElementsByClassName("photo-slide")
-  const dots = document.getElementsByClassName("dot")
-  slideIndex = selection > slides.length ? 1 :
-    selection < 1 ? slides.length
-      : selection
+  if (typeof document !== undefined) {
+    let index
+    const slides = document.getElementsByClassName("photo-slide")
+    const dots = document.getElementsByClassName("dot")
+    slideIndex = selection > slides.length ? 1 :
+      selection < 1 ? slides.length
+        : selection
 
-  for (index = 0; index < slides.length; index++) {
-    slides[index].style.display = "none"
+    for (index = 0; index < slides.length; index++) {
+      slides[index].style.display = "none"
+    }
+    for (index = 0; index < dots.length; index++) {
+      dots[index].className = dots[index].className.replace(" active", "")
+    }
+    if (slides[slideIndex - 1]) slides[slideIndex - 1].style.display = "block"
+    if (dots[slideIndex - 1]) dots[slideIndex - 1].className += " active"
+    clearTimeout(slideshowTimeout)
+    slideshowTimeout = setTimeout(goToAdjacentSlide, isUserClick ? 15000 : 5000)
   }
-  for (index = 0; index < dots.length; index++) {
-    dots[index].className = dots[index].className.replace(" active", "")
-  }
-  if (slides[slideIndex - 1]) slides[slideIndex - 1].style.display = "block"
-  if (dots[slideIndex - 1]) dots[slideIndex - 1].className += " active"
-  clearTimeout(slideshowTimeout)
-  slideshowTimeout = setTimeout(goToAdjacentSlide, isUserClick ? 15000 : 5000)
 }
 
 function goToAdjacentSlide(number = 1, isUserClick = false) {
@@ -41,8 +43,15 @@ function renderPhotoSlide(name, image) {
   </div>
 }
 
-export const PhotoGallery = () => (
-  <StaticQuery query={graphql`
+export class PhotoGallery extends Component {
+
+  componentDidMount() {
+    jumpToSlide(1)
+  }
+
+  render() {
+    return (
+      <StaticQuery query={graphql`
   query {
     neighbourhood: file(relativePath: { eq: "mobilio_neighbourhood.jpg" }) {
       childImageSharp {
@@ -109,29 +118,31 @@ export const PhotoGallery = () => (
     }
    }
     `} render={data => (
-    <div id={"photo-gallery-container"} className={'section-container'}>
-      <div>
-        <h1 className={"section-title"}>Gallery<span id={"photo-gallery"}/></h1>
-        <p>Mobilio Residents will experience the best of Vaughan's urban area, transit, shopping, and much more.</p>
-        <div id={"photo-container"}>
-          {Object.keys(data).map(photo =>
-            renderPhotoSlide(photo, data[photo].childImageSharp.fluid),
-          )}
-          <button className="prev-slide" onClick={() => goToAdjacentSlide(-1, true)}>&#10094;</button>
-          <button className="next-slide" onClick={() => goToAdjacentSlide(1, true)}>&#10095;</button>
-        </div>
-      </div>
-      <div id={"photo-navigator"}>
+        <div id={"photo-gallery-container"} className={"section-container"}>
+          <div>
+            <h1 className={"section-title"}>Gallery<span id={"photo-gallery"}/></h1>
+            <p>Mobilio Residents will experience the best of Vaughan's urban area, transit, shopping, and much more.</p>
+            <div id={"photo-container"}>
+              {Object.keys(data).map(photo =>
+                renderPhotoSlide(photo, data[photo].childImageSharp.fluid),
+              )}
+              <button className="prev-slide" onClick={() => goToAdjacentSlide(-1, true)}>&#10094;</button>
+              <button className="next-slide" onClick={() => goToAdjacentSlide(1, true)}>&#10095;</button>
+            </div>
+          </div>
+          <div id={"photo-navigator"}>
 
-        {
-          range(1, Object.keys(data).length, 1).map((element) =>
-            <button key={`d${element}`} className="dot" onClick={() => jumpToSlide(element, true)}/>,
-          )
-        }
-      </div>
-    </div>
-  )}/>
-)
+            {
+              range(1, Object.keys(data).length, 1).map((element) =>
+                <button key={`d${element}`} className="dot" onClick={() => jumpToSlide(element, true)}/>,
+              )
+            }
+          </div>
+        </div>
+      )}/>
+    )
+  }
+}
 
 
 
