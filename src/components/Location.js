e0a9fa8dae27740836310c9900b9d345c7b5950a
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-04-19 11:01:43 -0400

    Added scroll animation for location

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React, { Component } from "react"
 import "./Location.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSquare } from "@fortawesome/free-solid-svg-icons"
+import { setScrollAnimation } from "../helpers/scrollAnimation"
 
 function getKpi(digit, timescale, caption) {
   return <div className={"kpi-card"}>
@@ -12,25 +13,35 @@ function getKpi(digit, timescale, caption) {
   </div>
 }
 
-export const Location = () => (
-  <div id={"location-container"} className={'section-container'}>
-  <div id={"location-inner-container"}>
-    <h1 className={'section-title'}>Location<span id={'location'} /></h1>
-    <p>Just north of Toronto, a new neighbourhood in Vaughan Metropolitan Centre rises. Welcome to Charisma Condos, situated at
-      the epicentre of connectivity, convenience and innovation, and only steps away from Vaughan Mills. </p>
+export class Location extends Component {
 
-    <div id={"location-kpi"}>
-      {getKpi(1, 'min', 'Subway')}
-      {getKpi(1, 'min', 'Shopping')}
-      {getKpi(3, 'min', 'Highway')}
-      {getKpi(2, 'min', 'School')}
-      {getKpi(2, 'min', 'Recreation')}
-      {getKpi(2, 'min', 'Dining')}
-    </div>
+  componentDidMount() {
+    setScrollAnimation('kpi-card', 'scroll-animate')
+  }
 
-    <iframe title={"googlemaps"} height="450"
-            src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJvdrsxLkvK4gRg5m2IsjTerw&key=AIzaSyBwH66e9iE5lT856EiEJne_wcgBjJkh2uA"
-            allowFullScreen/>
-  </div>
-  </div>
-)
+  render() {
+    return (
+      <div id={"location-container"} className={"section-container"}>
+        <div id={"location-inner-container"}>
+          <h1 className={"section-title"}>Location<span id={"location"}/></h1>
+          <p>Just north of Toronto, a new neighbourhood in Vaughan Metropolitan Centre rises. Welcome to Charisma
+            Condos, situated at
+            the epicentre of connectivity, convenience and innovation, and only steps away from Vaughan Mills. </p>
+
+          <div id={"location-kpi"}>
+            {getKpi(1, "min", "Subway")}
+            {getKpi(1, "min", "Shopping")}
+            {getKpi(3, "min", "Highway")}
+            {getKpi(2, "min", "School")}
+            {getKpi(2, "min", "Recreation")}
+            {getKpi(2, "min", "Dining")}
+          </div>
+
+          <iframe title={"googlemaps"} height="450"
+                  src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJvdrsxLkvK4gRg5m2IsjTerw&key=AIzaSyBwH66e9iE5lT856EiEJne_wcgBjJkh2uA"
+                  allowFullScreen/>
+        </div>
+      </div>
+    )
+  }
+}
---
+++ src/helpers/scrollAnimation.js
@@ -0,0 +1,38 @@
+export function setScrollAnimation(elementClassName, animationClassName) {
+
+// get the element to animate
+  const elements = document.getElementsByClassName(elementClassName)
+  console.log(elements.length)
+// listen for scroll event and call animate function
+  document.addEventListener("scroll", animate)
+
+// check if element is in view
+  function inView(element) {
+    // get window height
+    const windowHeight = window.innerHeight
+    // get number of pixels that the document is scrolled
+    const scrollY = window.scrollY || window.pageYOffset
+
+    // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+    const scrollPosition = scrollY + windowHeight
+    // get element position (distance from the top of the page to the bottom of the element)
+
+    const elementPosition = element ? element.getBoundingClientRect().top + scrollY + element.clientHeight : 0;
+
+    // is scroll position greater than element position? (is element in view?)
+    return scrollPosition > elementPosition
+  }
+
+// animate element when it is in view
+  function animate() {
+    // is element in view?
+    Object.keys(elements).forEach(key => {
+      if (inView(elements[key])) {
+        // element is in view, add class to element
+        elements[key].classList.add(animationClassName)
+      } else {
+        elements[key].classList.remove(animationClassName)
+      }
+    })
+  }
+}
