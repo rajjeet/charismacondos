9207acba0c6fccecafbed138bfd9caf18c83ed3f
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-04-26 12:24:39 -0400

    Removed unused files. Modified seo keywords. Fixed logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#ef7635`,
         theme_color: `#65656a`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/Charisma_icon.png`, // This path is relative to the root of the site.
       },
     },
     {
