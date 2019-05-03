23e2cb2034281cd88088db7d75ed26d2e5e5f1bf
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-05-03 08:10:59 -0400

    Migrated Photo Gallery component to styled components

--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -2,10 +2,105 @@ import React, { Component } from "react"
 import "./PhotoGallery.css"
 import { graphql, StaticQuery } from "gatsby"
 import Img from "gatsby-image"
+import styled, {keyframes} from "styled-components"
+import * as theme from "../shared/theme"
 
 let slideIndex = 1
 let slideshowTimeout
 
+const fade = keyframes`
+  from {
+    opacity: .4
+  }
+  to {
+    opacity: 1
+  }
+`
+const S = {
+  PhotoGalleryContainer: styled.div`
+    text-align: center;
+    max-width: 80%;
+    margin: auto;
+    
+    .fade {
+      animation-name: ${fade};
+      animation-duration: 1.5s;
+    }
+  `,
+  PhotoContainer: styled.div`
+    height: 50vw;
+    width: 80vw;
+    max-width: 800px;
+    max-height: 550px;
+    position: relative;
+    overflow: hidden;
+    margin: 0 auto;
+    border-radius: 10px;
+  `,
+  SlideNavButton: styled.button`
+    cursor: pointer;
+    position: absolute;
+    top: 22px;
+    height: 100%;
+    width: 25%;
+    margin-top: -22px;
+    color: white;
+    font-weight: bold;
+    font-size: 18px;
+    transition: 0.6s ease;
+    user-select: none;
+    background: rgba(0, 0, 0, 0);
+    border: 0;
+    outline: none;
+    text-shadow: #cccccc 0 0 5px;
+    border-radius: 5px;
+
+    :hover {
+      background: rgba(0, 0, 0, .8);
+      @media (max-width: ${theme.computerBreakpoint}) {
+        background: rgba(0, 0, 0, 0);
+      }
+    }
+  `,
+  PhotoNavigator: styled.div`
+    margin: .5em auto;
+    width: 80%;
+    text-align: center;
+    
+    button {
+      cursor: pointer;
+      height: 20px;
+      width: 20px;
+      margin: 0em 2px;
+      background-color: white;
+      border: 1.5px solid gray;
+      border-radius: 50%;
+      display: inline-block;
+      transition: background-color 0.6s ease;
+      outline: none;
+  
+      @media (max-width: ${theme.mobileBreakpoint}) {
+        height: 15px;
+        width: 15px;
+      }
+      
+      &:hover {
+        background-color: grey;
+      }
+      
+      &.dot.active {
+      background-color: grey;
+      }
+    }
+  `,
+}
+
+S.LeftNavButton = styled(S.SlideNavButton)`
+    left: 0;
+`
+S.RightNavButton = styled(S.SlideNavButton)`
+    right: 0;
+`
 
 const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + (i * step))
 
@@ -53,7 +148,30 @@ export class PhotoGallery extends Component {
 
   render() {
     return (
-      <StaticQuery query={graphql`
+      <StaticQuery render={data => (
+        <S.PhotoGalleryContainer id={"photo-gallery-container"} className={"section-container"}>
+          <div>
+            <h1 className={"section-title"}>Gallery<span id={"photo-gallery"}/></h1>
+            <p>Charisma condo residents will experience the best of Vaughan's urban area, transit, shopping, and much
+              more.</p>
+            <S.PhotoContainer id={"photo-container"}>
+              {Object.keys(data).map(photo =>
+                renderPhotoSlide(photo, data[photo].childImageSharp.fluid),
+              )}
+              <S.LeftNavButton onClick={() => goToAdjacentSlide(-1, true)}>&#10094;</S.LeftNavButton>
+              <S.RightNavButton onClick={() => goToAdjacentSlide(1, true)}>&#10095;</S.RightNavButton>
+            </S.PhotoContainer>
+          </div>
+          <S.PhotoNavigator id={"photo-navigator"}>
+
+            {
+              range(1, Object.keys(data).length, 1).map((element) =>
+                <button key={`d${element}`} className="dot" onClick={() => jumpToSlide(element, true)}/>,
+              )
+            }
+          </S.PhotoNavigator>
+        </S.PhotoGalleryContainer>
+      )} query={graphql`
   query {
     picture1: file(relativePath: { eq: "construction-location.jpg" }) {
       childImageSharp {
@@ -175,30 +293,7 @@ export class PhotoGallery extends Component {
       }
     }    
    }
-    `} render={data => (
-        <div id={"photo-gallery-container"} className={"section-container"}>
-          <div>
-            <h1 className={"section-title"}>Gallery<span id={"photo-gallery"}/></h1>
-            <p>Charisma condo residents will experience the best of Vaughan's urban area, transit, shopping, and much
-              more.</p>
-            <div id={"photo-container"}>
-              {Object.keys(data).map(photo =>
-                renderPhotoSlide(photo, data[photo].childImageSharp.fluid),
-              )}
-              <button className="prev-slide" onClick={() => goToAdjacentSlide(-1, true)}>&#10094;</button>
-              <button className="next-slide" onClick={() => goToAdjacentSlide(1, true)}>&#10095;</button>
-            </div>
-          </div>
-          <div id={"photo-navigator"}>
-
-            {
-              range(1, Object.keys(data).length, 1).map((element) =>
-                <button key={`d${element}`} className="dot" onClick={() => jumpToSlide(element, true)}/>,
-              )
-            }
-          </div>
-        </div>
-      )}/>
+    `}/>
     )
   }
 }
