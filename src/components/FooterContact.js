82e1fa8c161333fc7ddf834a0e6d23074c2b947e
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-05-04 10:48:18 -0400

    Migrated Footer Contact to styled component

--- src/components/FooterContact.js
+++ src/components/FooterContact.js
@@ -1,10 +1,34 @@
 import React from "react"
-import "./FooterContact.css"
 import { ContactForm } from "./ContactForm"
+import styled from "styled-components"
+import * as theme from "../shared/theme"
+
+const S = {
+  FooterContactContainer: styled.div`
+    h1 {
+      text-align: center;
+    }
+    
+    background: ${theme.secondaryBackgroundColor};
+    
+    form {
+      max-width: 50%;
+      margin: 0 auto;
+    
+    @media (max-width: ${theme.tabletBreakpoint}) {
+        max-width: 80%;
+      }
+    
+    @media (max-width: ${theme.mobileBreakpoint}) {
+        max-width: 90%;
+      }
+    }
+`
+}
 
 export const FooterContact = () => (
-  <div id={'footer-contact-container'} className={'section-container'}>
+  <S.FooterContactContainer className={'section-container'}>
     <h1>Contact Us</h1>
     <ContactForm callToActionText={'Contact'}/>
-  </div>
+  </S.FooterContactContainer>
 )
