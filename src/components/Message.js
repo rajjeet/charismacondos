07b8932bea8a533c6b68665635f60d3fa5f7a5c1
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-05-04 10:56:11 -0400

    Migrated Message component to styled components

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,9 +1,30 @@
 import React from "react"
-import "./Message.css"
+import styled from "styled-components"
+import * as theme from "../shared/theme"
+
+const S = {
+  MessageContainer: styled.div`
+    position: fixed;
+    top: 1em;
+    right: 1em;
+    background: white;
+    box-shadow: 1px 1px 4px #444444;
+    width: 40%;
+    padding: 1em;
+    z-index: 4;
+    opacity: 0;
+    visibility: hidden;
+    transition: opacity .5s ease, visibility .5s ease;
+  
+    @media screen and (max-width: ${theme.tabletBreakpoint}) {
+      width: 70%;
+    }
+`
+}
 
 export const Message = () => (
-  <div id={'message-container'}>
+  <S.MessageContainer id={'message-container'}>
     <h6>Success!</h6>
     <p>Your submission was successful</p>
-  </div>
+  </S.MessageContainer>
 )
