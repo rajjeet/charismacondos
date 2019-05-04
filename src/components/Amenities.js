f75c35aa44cd3ab9b96201fecf6fa6903d6c2c1d
Rajjeet Phull <rajjeet.phull@gmail.com>
2019-05-04 09:35:00 -0400

    Migrated Amenities to styled components

--- src/components/Amenities.js
+++ src/components/Amenities.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import "./Amenities.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
   faFilm,
@@ -11,6 +10,86 @@ import {
   faRunning,
 } from "@fortawesome/free-solid-svg-icons"
 import { setScrollAnimation } from "../shared/scrollAnimation"
+import styled, {keyframes} from "styled-components"
+import * as theme from "../shared/theme"
+
+const animate = keyframes`
+  from {
+    transform: translateY(-2em);
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`
+
+
+const S = {
+  AmenitiesContainer: styled.div`
+    margin: 0 auto;
+    width: 80%;
+    text-align: center;
+  
+    @media (max-width: ${theme.tabletBreakpoint}) {
+      width: 100%;
+    }
+     .amenitiesBackIcon {
+      color: ${theme.sponsorColor}
+    }
+  
+    .amenitiesFrontIcon {
+      color: white;
+    }
+
+  .fa-layers {
+    min-height: 50px;
+    width: 40%;
+    text-align: left;
+    display: inline-block;
+    margin: 0 auto;
+    opacity: 0;
+
+    @media (max-width: ${theme.tabletBreakpoint}) {
+      margin: .2em auto;
+      width: 90%;
+    }
+    @media (max-width: ${theme.computerBreakpoint}) {
+      margin: .2em auto;
+      width: 70%;
+    }
+
+    svg:first-of-type {
+      margin: 0;
+    }
+
+    svg:last-of-type {
+      top: 8px;
+      left: 5px;
+      margin: 0;
+    }
+
+    .amenity-text {
+      position: relative;
+      top: 15px;
+      left: 55px;
+      width: 80%;
+      margin-right: 0;
+
+      @media (max-width: ${theme.tabletBreakpoint}) {
+        width: 80%;
+        top: 10px;
+        font-size: .8em;
+        display: inline-block;
+      }
+    }
+  }
+    
+  .scroll-animate {
+    animation: ${animate} .7s ease;
+    opacity: 1;
+  }
+`
+}
 
 function getAmenity(frontIcon, text) {
   return <div className="fa-layers">
@@ -27,7 +106,7 @@ export class Amenities extends Component {
 
   render() {
     return (
-      <div id={"amenities-container"} className={"section-container"}>
+      <S.AmenitiesContainer id={"amenities-container"} className={"section-container"}>
         <h1 className={"section-title"}>Amenities<span id={"amenities"}/></h1>
         <p>Live with luxury, convenience, and comfort</p>
         {getAmenity(faFilm, "Theatre Room")}
@@ -36,7 +115,7 @@ export class Amenities extends Component {
         {getAmenity(faRunning, "Virtual & Yoga/Aerobics Studios")}
         {getAmenity(faBaby, "Kids Play Room and Outdoor Play Area")}
         {getAmenity(faDumbbell, "Fitness Centre")}
-      </div>
+      </S.AmenitiesContainer>
     )
   }
 }
